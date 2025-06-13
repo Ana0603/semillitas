7121064d11f4518cde6c1f202d59b0e73a319322
Ana0603 <167488222+Ana0603@users.noreply.github.com>
2025-06-12 21:36:27 -0500

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import {Ingreso} from './pages/ingreso/ingreso';
+import {Inicio} from './pages/inicio/inicio';
+import {MiniJuego} from './pages/mini-juego/mini-juego';
+import {Vestuario} from './pages/vestuario/vestuario';
+import {Colecciones} from './pages/colecciones/colecciones';
+
+export const routes: Routes = [
+    { path: '', component: Ingreso },
+    { path: '', component: Inicio },
+    { path: '', component: MiniJuego },
+    { path: '', component: Vestuario },
+    { path: '', component: Colecciones },
+];
