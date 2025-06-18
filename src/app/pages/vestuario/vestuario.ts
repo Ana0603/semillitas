b04a2b4f681c5b5a21065bb468ef4af7b98a9bc7
Ana0603 <167488222+Ana0603@users.noreply.github.com>
2025-06-18 10:13:01 -0500

    Vestuario

--- src/app/pages/vestuario/vestuario.ts
+++ src/app/pages/vestuario/vestuario.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-vestuario',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './vestuario.html',
   styleUrl: './vestuario.css'
 })
 export class Vestuario {
+  ropaActual = '00.png';
+  ropaAnimada = false;
 
+  ropaOpciones = [
+    '01.png',
+    '02.png',
+    '03.png',
+    '04.png',
+    '05.png',
+    '06.png'
+  ];
+
+  cambiarRopa(nombreImagen: string) {
+    this.ropaAnimada = false;
+    this.ropaActual = nombreImagen;
+    setTimeout(() => {
+    this.ropaAnimada = true; // Aplica animación
+  }, 10);
+  }
 }
